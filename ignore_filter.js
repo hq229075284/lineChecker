e8d32b84f282eccdd9586acfd3c6eaffaa04a386
HanQ <229075284@qq.com>
2017-08-19 15:29:38 +0800

    修复对*.[ext]的支持

--- ignore_filter.js
+++ ignore_filter.js
@@ -22,54 +22,84 @@ const path = require("path");
  * @returns
  */
 function outPutReg(prefix = "", input = "") {
-  const startWithSlash = /^\//.test(input)
-  const endWithSlash = /\/$/.test(input)
+  const startWithSlash = /^\//.test(input);
+  const endWithSlash = /\/$/.test(input);
   // 含通配符
   if (/\*\*/.test(input)) {
     let _input,
-      type = 'ignore'
+      type = "ignore";
 
     if (/^!/.test(input)) {
       _input = input.substring(1);
-      type = 'check'
+      type = "check";
     } else {
-      _input = input
+      _input = input;
     }
 
-    const regular = new RegExp(path.join(prefix, _input + '$').replace(/[\\]/g, "\\\\").replace(/(\*\*)/g, "[\\w|\\W]+").replace(/\*/g, "[^\\\\|\\/]+").replace(/\./g, "\\."));
-    return {type: type, regular: regular};
-
+    const regular = new RegExp(
+      path
+        .join(prefix, _input + "$")
+        .replace(/[\\]/g, "\\\\")
+        .replace(/(\*\*)/g, "[\\w|\\W]+")
+        .replace(/\*/g, "[^\\\\|\\/]+")
+        .replace(/\./g, "\\.") + "$"
+    );
+    return { type: type, regular: regular };
   } else if (/\*/.test(input)) {
     let _input,
-      type = 'ignore'
+      type = "ignore";
 
     if (/^!/.test(input)) {
       _input = input.substring(1);
-      type = 'check'
+      type = "check";
     } else {
-      _input = input
+      _input = input;
     }
 
-    const regular = new RegExp(path.join(prefix, _input).replace(/[\\]/g, "\\\\").replace(/\*/g, "[^\\\\|\\/]+").replace(/\./g, "\\."));
-    return {type: type, regular: regular};
+    if (startWithSlash) {
+      const regular = new RegExp(
+        path
+          .join(prefix, _input)
+          .replace(/[\\]/g, "\\\\")
+          .replace(/\*/g, "[^\\\\|\\/]+")
+          .replace(/\./g, "\\$&") + "$"
+      );
+      return { type: type, regular: regular };
+    }
 
-  } else { // 不含通配符
+    const regular = new RegExp(
+      path
+        .join(prefix, _input)
+        .replace(/[\\]/g, "\\\\")
+        .replace(/\*/g, "[\\w|\\W]+")
+        .replace(/\./g, "\\$&") + "$"
+    );
+    return { type: type, regular: regular };
+  } else {
+    // 不含通配符
     let _input,
-      type = 'ignore'
+      type = "ignore";
 
     if (/^!/.test(input)) {
       _input = input.substring(1);
-      type = 'check'
+      type = "check";
     } else {
-      _input = input
+      _input = input;
     }
 
     if (startWithSlash && !endWithSlash) {
-      const regular = new RegExp(path.join(prefix, _input + '$').replace(/\\/g, "\\\\").replace(/\./g, "\\."));
-      return {type: type, regular: regular};
+      const regular = new RegExp(
+        path
+          .join(prefix, _input)
+          .replace(/\\/g, "\\\\")
+          .replace(/\./g, "\\$&") + "$"
+      );
+      return { type: type, regular: regular };
     }
-    const regular = new RegExp(path.join(prefix, _input).replace(/\\/g, "\\\\").replace(/\./g, "\\."));
-    return {type: type, regular: regular};
+    const regular = new RegExp(
+      path.join(prefix, _input).replace(/\\/g, "\\\\").replace(/\./g, "\\$&")
+    );
+    return { type: type, regular: regular };
   }
 }
 
